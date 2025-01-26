7089070d15ab750bbdf464ee72bb9e90a38775a2
Markus Hall <markus.hall@consiliari.de>
2025-01-26 18:05:21 +0100

    try fix compile script

--- compile_shader.cmake
+++ compile_shader.cmake
@@ -2,11 +2,15 @@ set(source ${SOURCE})
 set(output ${OUTPUT})
 set(dim ${DIM})
 
+
 get_filename_component(extension ${source} LAST_EXT)
 get_filename_component(output-dir ${output} DIRECTORY)
 file(MAKE_DIRECTORY ${output-dir})
 
 set(expanded ${output}.${dim}${extension})
+file(REMOVE ${output})
+file(REMOVE ${expanded})
+
 execute_process(COMMAND ${GLSLC} -E -DDEF_${dim} -o ${expanded} ${source})
 
 file(READ ${expanded} content)
