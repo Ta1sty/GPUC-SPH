7bc20baf8cf9d792416c788edd5946e170104c88
Markus Hall <markus.hall@consiliari.de>
2025-01-26 13:57:45 +0100

    fix folder

--- compile_shader.cmake
+++ compile_shader.cmake
@@ -4,6 +4,7 @@ set(dim ${DIM})
 
 get_filename_component(extension ${source} LAST_EXT)
 get_filename_component(output-dir ${output} DIRECTORY)
+file(MAKE_DIRECTORY ${output-dir})
 
 set(expanded ${output}.${dim}${extension})
 execute_process(COMMAND ${GLSLC} -E -DDEF_${dim} -o ${expanded} ${source})
